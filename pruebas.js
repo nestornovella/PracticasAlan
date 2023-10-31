e7f4a9236d61c2cb97177632d35eab1f1d6dd1db
AlanIbarra <alanproyect99@gmail.com>
2023-10-31 19:22:20 -0300

    change

--- pruebas.js
+++ pruebas.js
@@ -1,77 +1,124 @@
+function randomId() {
 
+    const letters = "AECDFKLIUP".split("")
+    const numbers = "1234567890".split("")
 
-const object = {
-    value1:2, 
-    value3:3,
-    value4:{
-        value1:9,
-        value2:{
-            value1:33,
-            value2:92
-        },
-        value3: 1900
-    },
-    value5:12
+    let id = ""
+    let changeCharacterType = false
+    for (let i = 1; i <= 4; i++) {
+        for (let x = 1; x <= 4; x++) {
+            if (!changeCharacterType) {
+                const character = letters[Math.floor(Math.random() * (letters.length))]
+                id += character
+            }else{
+                const character = numbers[Math.floor(Math.random() * (letters.length))]
+                id += character
+            }
+            changeCharacterType = !changeCharacterType
+
+        }
+        id += "-"
+    }
+    return id.slice(0, -1)
 }
 
-function getValue (object, number){
-    let itsFinded = false
-    for (const value in object){
-        if(typeof object[value] == 'object'){
-            const interObject = getValue(object[value], number)
-            if(interObject) itsFinded = true
-        }
-        else if(number === object[value]) itsFinded = true
+
+console.log(randomId())
+
+class User {
+    id = 0
+    nombre
+    edad
+    email
+    constructor(name, age, email) {
+        this.id++
+        this.nombre = name
+        this.edad = age
+        this.email = email
+    }
+
+    showInfo() {
+        return "nombre: " + this.nombre + "\n id: " + this.id
     }
     
-    return itsFinded
 }
 
-console.log(getValue(object, 1900))
-
-
-class Person{
-    name
-    age
-    surname
-    constructor(name, surname, age){
-        this.name = name,
-        this.age = age,
-        this.surname = surname
+class Vendedor extends User {
+    rango
+    antiguedad = 1
+    constructor(name, age, email, rango, antiguedad) {
+        super(name, age, email)
+        this.rango = rango
+        this.antiguedad = antiguedad
     }
-    showName(){
-        console.log(this.name + ' ' + this.surname)
+    
+}
+
+class Client extends User {
+    peliculasAlquiladas = []
+    direccion
+    telefono
+    membrecia
+    constructor(name, age, email, direccion, telefono, membrecia) {
+        super(name, age, email)
+        this.direccion = direccion
+        this.telefono = telefono
+        this.membrecia = membrecia
     }
+
+    alquilarPelicula (pelicula) {
+        if (pelicula.disponibility){
+            pelicula.changeDisponibility(false)
+            this.peliculasAlquiladas.push(pelicula)
+            
+        }
+        else {
+            console.error("La pelicula no está disponible")
+        }
+    }
+    
+}
+
+class Pelicula {
+    id
+    title
+    gender
+    parentalAdivisory
+    duration
+    disponibility
+    constructor(title, gender, parentalAdivisory, duration, disponibility) {
+        this.id = randomId()
+        this.title = title
+        this.gender = gender
+        this.parentalAdivisory = parentalAdivisory
+        this.duration = duration
+        this.disponibility = disponibility
+    }
+
+        changeDisponibility (boolean) {
+            this.disponibility = boolean
+        }
+
+        isAviable(){
+            if (this.disponibility) {
+                console.log ("Esta pelicula está disponible")
+            }
+            
+            else {
+                console.log ("Esta pelicula no está disponible")
+            }
+        }
+
 }
 
 
-const carlos = new Person('carlos', 'gonzales', 32)
-
-console.log(carlos)
-
-class PersonVip extends Person{
-    itsVIP
-    amigos = []
-    constructor(name, surname, age, itsVIP){
-        super(name, surname, age, itsVIP)
-        this.itsVIP = itsVIP
-    }
-
-    addFriend(nameFriend){
-        this.amigos.push(nameFriend)
-    }
-
-    getFriends(){
-        console.log(this.amigos)
-    }
 
 
 
-}
 
-const juan = new PersonVip('juan', 'gimenez', 32, true)
-juan.addFriend('pedro')
-juan.addFriend('ramon')
+const Alan = new Client ("Alan", 22, "alanska99@gmail.com", "Valparaiso 989", 1167413544, true)
+const horacio = new Vendedor("Horacio", 32, "horacioseller@gmail.com", "gerente", 2)
+const jurasicPark = new Pelicula ("Jurasic Park", "Action", !false, 2.45, !false)
 
-juan.getFriends()
-
+Alan.alquilarPelicula (jurasicPark)
+jurasicPark.isAviable()
